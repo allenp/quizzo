3ed8372093a3b23976cf8c540a3b9619f7ce5a24
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-08-06 10:36:11 +0100

    Fixed linting errors

--- inc/admin.php
+++ inc/admin.php
@@ -9,14 +9,14 @@ namespace Quizzo;
  * @return void
  */
 function register_quiz_columns( $columns ) {
-    unset( $columns['categories'] );
-    unset( $columns['date'] );
+	unset( $columns['categories'] );
+	unset( $columns['date'] );
 
-    $columns['questions']  = __( 'Questions', PLUGIN_DOMAIN );
-    $columns['categories'] = __( 'Categories', PLUGIN_DOMAIN );
-    $columns['date']       = __( 'Date', PLUGIN_DOMAIN );
+	$columns['questions']  = __( 'Questions', PLUGIN_DOMAIN );
+	$columns['categories'] = __( 'Categories', PLUGIN_DOMAIN );
+	$columns['date']       = __( 'Date', PLUGIN_DOMAIN );
 
-    return $columns;
+	return $columns;
 }
 
 /**
@@ -27,15 +27,15 @@ function register_quiz_columns( $columns ) {
  * @return void
  */
 function register_quiz_questions_column( $column, $post_id ) {
-    switch ( $column ) {
-        case 'questions' :
-            echo count( get_posts( array(
-                        'post_type'      => 'question',
-                        'post_status'    => 'publish',
-                        'posts_per_page' => -1,
-                        'meta_key'       => 'quizzo_quiz_id',
-                        'meta_value'     => $post_id,
-            ) ) );
-            break;
-    }
+	switch ( $column ) {
+		case 'questions' :
+			echo count( get_posts( array(
+						'post_type'      => 'question',
+						'post_status'    => 'publish',
+						'posts_per_page' => -1,
+						'meta_key'       => 'quizzo_quiz_id',
+						'meta_value'     => $post_id,
+			) ) );
+			break;
+	}
 }
