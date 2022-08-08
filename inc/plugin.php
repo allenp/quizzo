bc337c0ecc36f3df077001b9280ae774930251bf
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-08-08 10:34:43 +0100

    Fixed linting errors

--- inc/plugin.php
+++ inc/plugin.php
@@ -31,10 +31,10 @@ function quizzo_dashboard_cb() {
  */
 function quizzo_rewrite_flush() {
 	// Register CPTs
-    register_quizzo_cpts();
+	register_quizzo_cpts();
 
 	// Flush Rewrite Cache
-    flush_rewrite_rules();
+	flush_rewrite_rules();
 }
 
 /**
@@ -44,30 +44,30 @@ function quizzo_rewrite_flush() {
  * @return void
  */
 function register_quizzo_save_meta_box( $post_id ) {
-    // Check if user has permissions to save data.
-    if ( ! current_user_can( 'edit_post', $post_id ) ) {
-        return;
-    }
+	// Check if user has permissions to save data.
+	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+		return;
+	}
 
-    // Check if not an autosave.
-    if ( wp_is_post_autosave( $post_id ) ) {
-        return;
-    }
+	// Check if not an autosave.
+	if ( wp_is_post_autosave( $post_id ) ) {
+		return;
+	}
 
-    // Check if not a revision.
-    if ( wp_is_post_revision( $post_id ) ) {
-        return;
-    }
+	// Check if not a revision.
+	if ( wp_is_post_revision( $post_id ) ) {
+		return;
+	}
 
-    // Update Quiz
-    update_post_meta( $post_id, 'quizzo_quiz_id', $_POST['quizzo_quiz_id'] );
+	// Update Quiz
+	update_post_meta( $post_id, 'quizzo_quiz_id', $_POST['quizzo_quiz_id'] );
 
-    // Update Options
-    update_post_meta( $post_id, 'quizzo_option_1', $_POST['quizzo_option_1'] );
-    update_post_meta( $post_id, 'quizzo_option_2', $_POST['quizzo_option_2'] );
-    update_post_meta( $post_id, 'quizzo_option_3', $_POST['quizzo_option_3'] );
-    update_post_meta( $post_id, 'quizzo_option_4', $_POST['quizzo_option_4'] );
+	// Update Options
+	update_post_meta( $post_id, 'quizzo_option_1', $_POST['quizzo_option_1'] );
+	update_post_meta( $post_id, 'quizzo_option_2', $_POST['quizzo_option_2'] );
+	update_post_meta( $post_id, 'quizzo_option_3', $_POST['quizzo_option_3'] );
+	update_post_meta( $post_id, 'quizzo_option_4', $_POST['quizzo_option_4'] );
 
-    // Update Answer
-    update_post_meta( $post_id, 'quizzo_answer', $_POST['quizzo_answer'] );
+	// Update Answer
+	update_post_meta( $post_id, 'quizzo_answer', $_POST['quizzo_answer'] );
 }
