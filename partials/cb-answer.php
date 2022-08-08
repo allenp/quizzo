5706db1207abb71d9207f4ca90cc399590b2ae02
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-08-08 09:23:28 +0100

    Fixed callback template for answer metabox

--- partials/cb-answer.php
+++ partials/cb-answer.php
@@ -1,3 +1,5 @@
+<?php global $answers, $quiz_answer; ?>
+
 <select class="quizzo_select" name="quizzo_answer">
 	<?php foreach ( $answers as $key => $value ) : ?>
 		<option value="<?php echo esc_attr( $key ); ?>" <?php selected( $quiz_answer, esc_attr( $key ) ); ?>>
