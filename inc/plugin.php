0bcbfc8e31d82f83eaaf56d7133e804f098fdeb4
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-08-16 08:45:20 +0100

    Added publish_question callback method

--- inc/plugin.php
+++ inc/plugin.php
@@ -36,3 +36,38 @@ function quizzo_rewrite_flush() {
 	// Flush Rewrite Cache
     flush_rewrite_rules();
 }
+
+/**
+ * Publish Question Callback Method
+ *
+ * @param int $post_id
+ * @return void
+ */
+function register_quizzo_save_meta_box( $post_id ) {
+    // Check if user has permissions to save data.
+    if ( ! current_user_can( 'edit_post', $post_id ) ) {
+        return;
+    }
+
+    // Check if not an autosave.
+    if ( wp_is_post_autosave( $post_id ) ) {
+        return;
+    }
+
+    // Check if not a revision.
+    if ( wp_is_post_revision( $post_id ) ) {
+        return;
+    }
+
+    // Update Quiz
+    update_post_meta( $post_id, 'quizzo_quiz_id', $_POST['quizzo_quiz_id'] );
+
+    // Update Options
+    update_post_meta( $post_id, 'quizzo_option_1', $_POST['quizzo_option_1'] );
+    update_post_meta( $post_id, 'quizzo_option_2', $_POST['quizzo_option_2'] );
+    update_post_meta( $post_id, 'quizzo_option_3', $_POST['quizzo_option_3'] );
+    update_post_meta( $post_id, 'quizzo_option_4', $_POST['quizzo_option_4'] );
+
+    // Update Answer
+    update_post_meta( $post_id, 'quizzo_answer', $_POST['quizzo_answer'] );
+}
