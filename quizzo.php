31caec5d5aa84474537c1a9bc3fc086f09a2c1ff
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-08-18 08:35:51 +0100

    Added rewrite flush

--- quizzo.php
+++ quizzo.php
@@ -34,8 +34,11 @@ add_action( 'admin_menu', __NAMESPACE__ . '\register_quizzo_menu', 9 );
 add_action( 'add_meta_boxes', __NAMESPACE__ . '\register_quizzo_meta_boxes' );
 add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\register_quizzo_css' );
 
-add_filter( 'manage_quiz_posts_columns', 'register_quiz_columns' );
-add_action( 'manage_quiz_posts_custom_column' , 'register_quiz_questions_column', 10, 2 );
+add_filter( 'manage_quiz_posts_columns', __NAMESPACE__ . 'register_quiz_columns' );
+add_action( 'manage_quiz_posts_custom_column' , __NAMESPACE__ . 'register_quiz_questions_column', 10, 2 );
 
 // Plugin shortcode
 add_shortcode( 'quizzo', __NAMESPACE__ . '\quizzo_shortcode' );
+
+// Rewrite flush
+register_activation_hook( __FILE__, __NAMESPACE__ . 'quizzo_rewrite_flush' );
