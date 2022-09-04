d64b21c9892dcf97c485b6aea22fdc9c0e5e1081
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-04 23:09:24 +0100

    Fixed linting errors

--- inc/menu.php
+++ inc/menu.php
@@ -8,22 +8,22 @@ namespace Quizzo;
  * @return void
  */
 function register_quizzo_menu() {
-    add_menu_page(
-        __( 'Quizzo', PLUGIN_DOMAIN ),
-        'Quizzo',
-        PLUGIN_ROLE,
-        PLUGIN_SLUG,
-        false,
-        'dashicons-edit-page',
-        ''
-    );
+	add_menu_page(
+		__( 'Quizzo', PLUGIN_DOMAIN ),
+		'Quizzo',
+		PLUGIN_ROLE,
+		PLUGIN_SLUG,
+		false,
+		'dashicons-edit-page',
+		''
+	);
 
-    add_submenu_page(
-        PLUGIN_SLUG,
-        'Quizzo',
-        'Dashboard',
-        PLUGIN_ROLE,
-        PLUGIN_SLUG,
-        'quizzo_plugin_page',
-    );
+	add_submenu_page(
+		PLUGIN_SLUG,
+		'Quizzo',
+		'Dashboard',
+		PLUGIN_ROLE,
+		PLUGIN_SLUG,
+		'quizzo_plugin_page',
+	);
 }
