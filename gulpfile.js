1d41b7ef03c8a4591a8cd756e8a31296092e94b6
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-20 19:31:00 +0100

    Added gulpfile.js for task runner

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+// Get Gulp module
+const { src, dest, watch } = require('gulp');
+
+//Get minify, autoprefixer and sass modules
+const minify = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+
+//Set up paths
+const paths = {
+	src: {
+		css: './assets/css/src/**/*.scss',
+		js: './assets/js/src/**/*.js'
+	},
+	dest: {
+		css: './assets/css/dist/css',
+		js: './assets/js/dist/js'
+	}
+}
+
+// Compile script
+const compile = () => {
+	return src(paths.src.css)
+		.pipe(sass())
+		.pipe(autoprefixer())
+		.pipe(minify())
+		.pipe(dest(paths.dest.css));
+}
+
+// Watch script
+const observe = () => {
+	watch(paths.src.css, compile);
+}
+
+exports.sass = compile;
+exports.watch = observe;
