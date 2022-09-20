5bb0dd55164fef33b4e8a4ef47df5d1a8c38b5b8
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-20 20:17:18 +0100

    Set up plugin entry point file

---
+++ quizzo.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Quizzo
+ * Plugin URI:  https://github.com/chigozieorunta/quizzo
+ * Description: A simple plugin to help you set up Quizzes behind a PayWall.
+ * Version:     1.0.0
+ * Author:      Chigozie Orunta
+ * Author URI:  https://chigozieorunta.com
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: quizzo
+ * Domain Path: /languages
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
