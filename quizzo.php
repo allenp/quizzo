86951d096638e06df3abfb35645decc73fa968ee
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-20 20:25:01 +0100

    Enqueue CSS file to be used within WP Admin

--- quizzo.php
+++ quizzo.php
@@ -19,3 +19,21 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'PLUGIN_SLUG', 'quizzo' );
 define( 'PLUGIN_ROLE', 'manage_options' );
 define( 'PLUGIN_DOMAIN', 'quizzo' );
+
+/**
+ * WP Action Hook
+ * admin_enqueue_scripts | register_quizzo_css
+ */
+add_action( 'admin_enqueue_scripts', 'register_quizzo_css' );
+
+/**
+ * Enqueue Quizzo Plugin CSS file
+ *
+ * @return void
+ */
+function register_quizzo_css() {
+    wp_enqueue_style(
+        PLUGIN_SLUG,
+        plugin_dir_url( __FILE__ ) . './assets/css/dist/quizzo.css'
+    );
+}
