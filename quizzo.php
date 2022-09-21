1790c83eea1201ab9848911a8b2abde71b9b5da4
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-21 08:50:21 +0100

    Added namespace to publish_question hook

--- quizzo.php
+++ quizzo.php
@@ -31,8 +31,9 @@ foreach( glob( __DIR__ . "/inc/*.php" ) as $file ) {
 // Plugin hooks
 add_action( 'init', __NAMESPACE__ . '\register_quizzo_cpts' );
 add_action( 'admin_menu', __NAMESPACE__ . '\register_quizzo_menu', 9 );
-add_action( 'add_meta_boxes', __NAMESPACE__ . '\register_quizzo_meta_boxes' );
 add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\register_quizzo_css' );
+add_action( 'add_meta_boxes', __NAMESPACE__ . '\register_quizzo_meta_boxes' );
+add_action( 'publish_question', __NAMESPACE__ . 'register_quizzo_save_meta_box' );
 
 add_filter( 'manage_quiz_posts_columns', __NAMESPACE__ . 'register_quiz_columns' );
 add_action( 'manage_quiz_posts_custom_column' , __NAMESPACE__ . 'register_quiz_questions_column', 10, 2 );
