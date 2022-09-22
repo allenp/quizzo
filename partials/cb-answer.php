9ef7eae82695abd4dc857747e4b5d32457399844
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-22 13:39:52 +0100

    Changed CSS class for select tag

--- partials/cb-answer.php
+++ partials/cb-answer.php
@@ -1,6 +1,6 @@
 <?php global $answers, $quiz_answer; ?>
 
-<select class="quizzo_select" name="quizzo_answer">
+<select class="widefat" name="quizzo_answer">
 	<?php foreach ( $answers as $key => $value ) : ?>
 		<option value="<?php echo esc_attr( $key ); ?>" <?php selected( $quiz_answer, esc_attr( $key ) ); ?>>
 			<?php echo esc_html( $value ); ?>
