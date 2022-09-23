0442d5a14d51807ba13d5bab0060fc2870ae321a
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-23 23:59:56 +0100

    Modified template for Answer metabox

--- partials/cb-answer.php
+++ partials/cb-answer.php
@@ -1,7 +1,7 @@
-<?php global $answers, $quiz_answer; ?>
+<?php global $quiz_options, $quiz_answer; ?>
 
 <select class="widefat" name="quizzo_answer">
-	<?php foreach ( $answers as $key => $value ) : ?>
+	<?php foreach ( $quiz_options as $key => $value ) : ?>
 		<option value="<?php echo esc_attr( $key ); ?>" <?php selected( $quiz_answer, esc_attr( $key ) ); ?>>
 			<?php echo esc_html( $value ); ?>
 		</option>
