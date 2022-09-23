852b45e68404f4ed30748104e3ca27a52594bb9e
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-23 10:45:07 +0100

    Modified session counter logic and added PHPDoc

--- inc/ajax.php
+++ inc/ajax.php
@@ -2,6 +2,11 @@
 
 namespace Quizzo;
 
+/**
+ * Save User's Answer
+ *
+ * @return void
+ */
 function save_user_answer() {
 	//Get IDs
 	$answer_id   = $_POST['answer_id'];
@@ -14,15 +19,16 @@ function save_user_answer() {
 	//Get next question
 	if ( isset( $_SESSION['questions_ids'] ) ) {
 		$index = array_search( $question_id, $_SESSION['questions_ids'] );
-		$index = $index + 1;
-		$_SESSION['question_counter'] = $_SESSION['questions_ids'][$index];
+
+		//Check if its the last index, if not, update.
+		if ( $index < count ( $_SESSION['questions_ids'] ) ) {
+			$_SESSION['question_counter'] = $index + 1;
+		}
 	}
 
 	//Prepare result
 	$result['status']   = $compare;
 	$result['question'] = $answer;
-	$result['index']    = $index;
-	$result['counter']  = $_SESSION['question_counter'];
 
 	//Encode and send
 	$result = json_encode($result);
