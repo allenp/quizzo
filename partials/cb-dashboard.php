94fa467fd9db34632d53eff8cc937fd3373131a9
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-25 20:12:45 +0100

    New Dasbhoard template page

--- partials/cb-dashboard.php
+++ partials/cb-dashboard.php
@@ -1,4 +1,48 @@
 <section class="wrap quizzo">
-	<h1>Quizzo</h1>
-	<p>A simple plugin to help you set up Multiple Choice Questions Quizzes behind a PayWall.</p>
+	<header>
+		<h1>Quizzo</h1>
+		<p>A simple plugin to help you set up Quizzes behind a WooCommerce Paywall.</p>
+		<span>Developer: <a href="https://linkedin.com/in/chigozieorunta">Chigozie Orunta</a></span>
+		<span>Plugin Repo: <a href="https://github.com/chigozieorunta/quizzo">Github</a></span>
+	</header>
+
+	<main>
+		<h2>Features</h2>
+		<p>Quizzo is the perfect tool for setting up a simple Quiz. Here are a list of its features:</p>
+		<div class="columns">
+			<div>
+				<span class="dashicons dashicons-edit-page"></span>
+				<h3>Quizzes</h3>
+				<p>Easy set up for Quizzes, Questions and Answers.</p>
+			</div>
+			<div>
+				<span class="dashicons dashicons-cart"></span>
+				<h3>WooCommerce</h3>
+				<p>Enable Quiz purchase via WooCommerce for each user.</p>
+			</div>
+			<div>
+				<span class="dashicons dashicons-yes"></span>
+				<h3>Scores</h3>
+				<p>Capture Score data and user info for each Quiz session.</p>
+			</div>
+			<div>
+				<span class="dashicons dashicons-update"></span>
+				<h3>Page by Page</h3>
+				<p>Step-by-Step page feature for each Quiz question.</p>
+			</div>
+			<div>
+				<span class="dashicons dashicons-clock"></span>
+				<h3>Timer/Counter</h3>
+				<p>Timer/Counter feature for each Quiz question.</p>
+			</div>
+			<div>
+				<span class="dashicons dashicons-database"></span>
+				<h3>Custom Data</h3>
+				<p>Meta Data capturing for Quizzes, Questions and Answers.</p>
+			</div>
+		</div>
+		<p id="get-started">
+			<a href="<?php echo home_url(); ?>/wp-admin/edit.php?post_type=quiz">Get Started</a>
+		<p>
+	</main>
 </section>
