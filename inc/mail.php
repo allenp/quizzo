92f610dd3a04aa253ae55f67b8c70ab3370709a8
Chigozie Orunta <chigozieorunta@yahoo.com>
2022-09-29 15:14:36 +0100

    Added PHPDoc block

--- inc/mail.php
+++ inc/mail.php
@@ -2,6 +2,15 @@
 
 namespace Quizzo;
 
+/**
+ * Attachment email
+ *
+ * @param object $order
+ * @param bool $sent_to_admin
+ * @param string $plain_text
+ * @param string $email
+ * @return void
+ */
 function attach_quizzo_link_to_wc_email( $order, $sent_to_admin, $plain_text, $email ) {
 	foreach ( $order->get_items() as $item ) {
 		// Get product details
@@ -14,42 +23,18 @@ function attach_quizzo_link_to_wc_email( $order, $sent_to_admin, $plain_text, $e
 
 		// Get Quiz page ID, Quiz link
 		global $wp, $wpdb;
-		$sql = 'SELECT ID FROM '. $wpdb->posts. ' WHERE post_content LIKE "%' . $quiz_shortcode . '%"';
+		$sql = 'SELECT ID FROM ' . $wpdb->posts . ' WHERE post_status="publish" AND post_content LIKE "%' . $quiz_shortcode . '%"';
 		$page_id   = $wpdb->get_var( $sql );
 		$quiz_link = get_permalink( $page_id );
 
 		// Attach if it is a Quizzo product
 		if ( $product->is_type('quizzo') ) {
 			$quiz_message = sprintf(
-				'<p>You can take your test quiz, %2$s, %3$s, %4$s, %5$s <a href="%1$s">here</a>.</p>',
-				$quiz_link,
-				$quiz_id,
-				$quiz_shortcode,
-				$sql,
-				$page_id
+				'<p>You can take your test quiz <a href="%1$s">here</a>.</p>',
+				$quiz_link
 			);
 			$plain_text .= $quiz_message;
         }
 	}
 	echo $plain_text;
-}
-
-function get_shortcode_pages_ids( $shortcode ) {
-	// Prepare ID array
-	$page_ids = [];
-
-	// Get Query
-	$query = new WP_Query( array(
-		's' => $shortcode
-	) );
-
-	// Get the IDs
-	if ( $query->have_posts() ) {
-		while ( $query->have_posts() ) {
-			$query->the_post();
-			$page_ids[] = get_the_ID();
-		}
-	}
-
-	return $page_ids[0];
 }
